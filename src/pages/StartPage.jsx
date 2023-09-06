6f8bc03f30e39c2a006fd017c3bea35c1fb7cfd5
hyeseung <wjoo3@naver.com>
2023-09-06 23:14:56 +0900

    feat : 마우스 따라다니는 원 생성

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,7 +1,27 @@
-import { Link } from "react-router-dom";
+import { useCallback, useState } from "react";
 
 const StartPage = () => {
-  return <Link to={"index"}>idex page로 이동</Link>;
+  // 마우스 x,y 좌표를 담는 state
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  // 마우스 움직일 때 x,y 좌표 변경하는 함수
+  const setMousePosition = useCallback((e) => {
+    setX(e.clientX);
+    setY(e.clientY);
+  }, []);
+
+  return (
+    <div className="container" onMouseMove={setMousePosition}>
+      <div
+        className="circle"
+        style={{
+          top: y,
+          left: x,
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default StartPage;
