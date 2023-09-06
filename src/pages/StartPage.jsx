a9617e583178f4afb4480bdf71ae361653e10f45
hyeseung <wjoo3@naver.com>
2023-09-07 01:07:41 +0900

    feat : 세부 내용 추가

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,33 +1,46 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { ReactComponent as ArrowSvg } from "../assets/icon/arrow_black.svg";
+import { useNavigate } from "react-router-dom";
 
 const StartPage = () => {
+  const navigate = useNavigate();
+
   // 마우스 x,y 좌표를 담는 state
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
 
   // 마우스 움직일 때 x,y 좌표 변경하는 함수
   const setMousePosition = useCallback((e) => {
-    setX(e.clientX);
-    setY(e.clientY);
+    setMouseX(e.clientX);
+    setMouseY(e.clientY);
   }, []);
+
+  useEffect(() => {}, []);
 
   return (
     <div className="container" onMouseMove={setMousePosition}>
-      <div className="arrow-wrap">
-        <ArrowSvg className="arrow-icon" />
+      <div to={"index"} className="arrow-wrap">
+        <ArrowSvg className="arrow-icon" onClick={() => navigate("index")} />
       </div>
       <div
         className="circle"
         style={{
-          top: y,
-          left: x,
+          top: mouseY,
+          left: mouseX,
         }}
       ></div>
       <div className="title">2023</div>
       <div className="title">FRONTEND</div>
       <div className="title">PORTFOLIO</div>
+      <div className="content1">2023 PORFOLIO</div>
+      <div className="content2">
+        Ajou Univ.
+        <br />
+        Digital Media & Software
+        <br />
+        Hyeseung Lee
+      </div>
     </div>
   );
 };
