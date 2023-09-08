7e93a3eea2a959f4151d0b7ac135641f4b7b471a
hyeseung <wjoo3@naver.com>
2023-09-09 00:14:24 +0900

    docs : scss import 위치 변경

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,14 +1,13 @@
-import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowSvg } from "../assets/icon/arrow_black.svg";
+
+import "../styles/indexPage.scss";
 
 const IndexPage = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      <button onClick={() => navigate(-1)}>뒤로가기</button>
-      <Link to={"1"}>목차1</Link>
-      <Link to={"2"}>목차2</Link>
-      <Link to={"3"}>목차3</Link>
+    <div className="container">
+      <div className="back-btn-wrap">
+        <ArrowSvg className="arrow-icon" />
+      </div>
     </div>
   );
 };
