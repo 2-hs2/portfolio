2c14d4e2cf6e40bb521a5ba259a8d0a0f3c19085
hyeseung <wjoo3@naver.com>
2023-09-09 00:00:24 +0900

    chore : 목차 페이지에 대한 scss 파일 생성 및 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./styles/startPage.scss";
+import "./styles/indexPage.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
