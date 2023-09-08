ea9914cddea907ecc1aaf0f26058203d31dd5643
hyeseung <wjoo3@naver.com>
2023-09-09 00:44:10 +0900

    feat : 전체 배경색 지정 state 추가 및 화살표 아이콘에 적용

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,13 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 import { ReactComponent as ArrowSvg } from "../assets/icon/arrow_black.svg";
 
 const IndexPage = () => {
   const navigate = useNavigate();
 
+  // container 배경색 지정 state
+  const [backgroundColor, setBackgroundColor] = useState("#fff");
+
   return (
-    <div className="container2">
+    <div className="container2" style={{ backgroundColor: backgroundColor }}>
       <div className="arrow-wrap">
-        <ArrowSvg className="arrow-icon" onClick={() => navigate("/")} />
+        <ArrowSvg
+          className="arrow-icon"
+          onClick={() => navigate("/")}
+          onMouseOver={() => setBackgroundColor("#000")}
+          onMouseLeave={() => setBackgroundColor("#fff")}
+        />
       </div>
     </div>
   );
