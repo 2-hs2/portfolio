422c9daef395ce07083f2e8a271630b400c45940
hyeseung <wjoo3@naver.com>
2023-09-30 23:16:18 +0900

    feat : 상세 내용 대표 이미지 left area로 이동

--- src/components/detailPage/ContentLeftArea.jsx
+++ src/components/detailPage/ContentLeftArea.jsx
@@ -1,7 +1,10 @@
-const ContentLeftArea = ({ number, title, subtitle }) => {
+const ContentLeftArea = ({ number, title, subtitle, img }) => {
   return (
     <div className="left-wrap">
       <div className="page-number">{number}</div>
+      <div className="img-wrap">
+        <img src={img} className="img" />
+      </div>
       <div className="left-bottom-wrap">
         <div className="page-title">{title}</div>
         <div className="page-subtitle">{subtitle}</div>
