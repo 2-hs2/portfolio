301c3362954e7e1315c44d8cd617e4d1ddbe872d
hyeseung <wjoo3@naver.com>
2023-10-01 00:19:00 +0900

    feat : content section 좌측 영역 이미지 사이즈

--- src/components/detailPage/ContentLeftArea.jsx
+++ src/components/detailPage/ContentLeftArea.jsx
@@ -1,13 +1,13 @@
 const ContentLeftArea = ({ number, title, subtitle, img }) => {
   return (
     <div className="left-wrap">
-      <div className="page-number">{number}</div>
-      <div className="img-wrap">
-        <img src={img} className="img" />
-      </div>
-      <div className="left-bottom-wrap">
+      <div className="top-wrap">
+        <div className="page-number">{number}</div>
         <div className="page-title">{title}</div>
         <div className="page-subtitle">{subtitle}</div>
+      </div>
+      <div className="img-wrap">
+        <img src={img.src} className="img" style={{ width: img.size }} />
       </div>
     </div>
   );
