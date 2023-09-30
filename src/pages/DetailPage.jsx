92871ae828cad3ea8fa0b7e6c3286f0db0477825
hyeseung <wjoo3@naver.com>
2023-10-01 00:20:29 +0900

    feat : detail page에 래브라도 인턴 상세 내용 section 적용

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -3,16 +3,22 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ReactComponent as ArrowSvg } from "../assets/icon/arrow_black.svg";
 
 import TitleSection from "../components/detailPage/TitleSection";
+import ContentSection1 from "../components/detailPage/ContentSection1";
 
 const DetailPage = () => {
   const param = useParams();
   const navigate = useNavigate();
 
   return (
-    <div className="container3">
-      <ArrowSvg className="arrow-icon" onClick={() => navigate("/index")} />
-      <TitleSection index={param.indexId} />
-    </div>
+    <>
+      <div className="container3">
+        <ArrowSvg className="arrow-icon" onClick={() => navigate("/index")} />
+        <TitleSection index={param.indexId} />
+      </div>
+      <div className="container4">
+        {param.indexId == 1 && <ContentSection1 index={param.indexId} />}
+      </div>
+    </>
   );
 };
 
