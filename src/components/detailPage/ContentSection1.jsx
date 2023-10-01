d979dc4c3e45e8fb635a0c8999dd9dc4ee649353
hyeseung <wjoo3@naver.com>
2023-10-01 17:06:43 +0900

    feat : 파란학기제 상세 내용 추가

--- src/components/detailPage/ContentSection1.jsx
+++ src/components/detailPage/ContentSection1.jsx
@@ -18,7 +18,7 @@ import ContentLeftArea from "./ContentLeftArea";
 import ContentRightArea from "./ContentRightArea";
 import ContentMiddleArea from "./ContentMiddleArea";
 
-const ContentSection = ({ index }) => {
+const ContentSection = () => {
   const [page, setPage] = useState(0);
   const [pagination, setPagination] = useState([]);
 
@@ -54,7 +54,7 @@ const contentData = [
     desc: `"Labrador 스캐너를 이용하여 소프트웨어 소스코드, zip 파일 또는 CLI를 분석한 결과들이 나오는 페이지로, 공통 취약점 등급 시스템인 CVSS의 두 가지 버전과 래브라도만의 등급 시스템인 LPP 총 세 가지 버전에 따라 소프트웨어 명세서인 SBOM, 취약점과 라이선스 수, patch가 필요한 취약점과 조치가 필요한 라이선스의 정보를 한 눈에 확인할 수 있다. 또한 헤더를 통해 다른 프로젝트들과 비교하는 '분석 비교 페이지'로의 이동과 비교 횟수를 확인할 수 있으며 해당 프로젝트에 대한 분서을 여러번 반복할 수 있다."`,
     process: `UI 디자인 > 구조 및 스타일 작업 > React 컴포넌트 작업 > React Hook을 이용한 데이터 적용 > UI 디자인 수정`,
     design: `피그마를 툴로 사용하여 분석요약 페이지의 헤더, SBOM 섹션, Risk 섹션, Patch 섹션, 정책 섹션의 디자인을 진행하였다. Patch 섹션과 Risk 섹션의 경우 여러 디자인 안들을 제시하여 프론트팀 내에서 팀원들과의 소통을 통해 결정하였으며 개발이 진행된 후 SBOM, Risk, Patch 섹션에 대한 2차 수정 또한 진행되어 더 나은 방향의 UI를 만들고자 하였다.`,
-    designToolImg: FigmaImg,
+    designToolImg: [{ src: FigmaImg, size: "6%" }],
     colors: [
       { name: "Labrador Navy", hex: ["#214172"] },
       {
@@ -84,7 +84,7 @@ const contentData = [
     desc: `기존 분석 프로젝트와 타겟 분석 프로젝트를 비교하여 컴포넌트, 취약점, 라이선스에 대한 변경사항들을 비교해주는 페이지이다. 기존의 UI의 경우 분석요약 페이지에서의 팝업 형식이었지만 중요도가 높아짐에 따라 페이지로 따로 분리되어 디자인이 새롭게 리뉴얼되었다. 작동의 경우 분석할 프로젝트를 선택하고 비교하기 버튼을 누르면 해당 타겟 분석 프로젝트에 대해 기존 분석 프로젝트의 컴포넌트, 취약점, 라이선스 변화를 탭 영역에서 간단하게 확인할 수 있으며, 탭 영역에서 원하는 버튼 선택시 테이블 영역에 보다 자세한 정보들이 출력되는 형식으로 이루어진다.`,
     process: `1차 UI 디자인 > 2차 UI 디자인 > 최종 UI 디자인 > 퍼블리싱 > 로직 작업`,
     design: `피그마를 툴로 사용하여 1차적으로 두 가지 타입의 영역 스타일로 총 5가지 안을 제작하였으며 2차를 거쳐 최종적으로 3안의 셀렉터 영역과 4안의 탭 영역이 합쳐진 디자인이 선정되었다.`,
-    designToolImg: FigmaImg,
+    designToolImg: [{ src: FigmaImg, size: "6%" }],
     colors: [
       { name: "Labrador Navy", hex: ["#214172"] },
       {
@@ -112,7 +112,7 @@ const contentData = [
     desc: `래브라도의 각 페이지들에 출력되는 UI와 공통 컴포넌트들에 대한 스타일 코드를 확인하여 그대로 피그마 내에 정리한다. 단순 정리 뿐만 아니라 코드 상에 중첩이나 출력이 적합하게 이루어지지 않는 UI 혹은 컴포넌트를 찾아 문제점을 지적하고 코드 반영 우선순위 중 '수정'과 '반영'에 해당하는 부분에 대한 코드 수정을 진행한다. 또한 박스, 토글 같은 공통 규격이 필요한 컴포넌트의 경우 React 컴포넌트화를 통해 통일화한다.`,
     process: `UI / 컴포넌트 정리 > 문제점에 대한 코멘트 추가 > '수정'과 '반영'에 대한 코드 수정 및 React 컴포넌트화`,
     design: `피그마를 툴로 사용하여 래브라도 내의 각 페이지별에 존재하는 UI와 공통 컴포넌트들을 정리하고 코멘트 툴을 이용하여 문제점이나 수정하면 좋을 부분에 대한 코멘트를 작성한다.`,
-    designToolImg: FigmaImg,
+    designToolImg: [{ src: FigmaImg, size: "6%" }],
     colors: [{ name: "래브라도 내에서 사용되는 모든 컬러들", hex: [] }],
     font: "Noto Sans KR",
     techStackImg: [
@@ -132,7 +132,7 @@ const contentData = [
     desc: `기존 삼성화재 IVAS 개발이 기능 위주로 이루어지다 보니 UI 디자인 측면에서 다소 투박하고 비효율적인 배치의 모습을 확인할 수 있다. 따라서 이러한 측면을 개선하기 위해 헤더, 분석대상추가, 통합점검 셀렉터, 취약점 조치현황, 분석결과, 인풋, 뒤로가기/페이지 목록, 탭메뉴 총 8개의 UI의 디자인 수정을 진행하였다.`,
     process: `UI / 컴포넌트 정리 > 문제점에 대한 코멘트 추가 > '수정'과 '반영'에 대한 코드 수정 및 React 컴포넌트화`,
     design: `피그마를 툴로 사용하여 총 8가지의 UI에 대한 수정이 이루어졌다. 하나씩 순차적으로 진행하며 2차 3차 피드백을 받으며 나은 방향으로 UI 디자인을 수정하고자 하였다.`,
-    designToolImg: FigmaImg,
+    designToolImg: [{ src: FigmaImg, size: "6%" }],
     colors: [
       { name: "IVAS Blue", hex: ["#3b5bdb", "#4f6bdf"] },
       {
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as ArrowSvg } from "../assets/icon/arrow_black.svg";
 
 import TitleSection from "../components/detailPage/TitleSection";
 import ContentSection1 from "../components/detailPage/ContentSection1";
+import ContentSection3 from "../components/detailPage/ContentSection3";
 import LinkSection from "../components/detailPage/LinkSection";
 
 const DetailPage = () => {
@@ -18,11 +19,10 @@ const DetailPage = () => {
       </div>
       <div className="container4">
         {param.indexId == 1 && <ContentSection1 index={param.indexId} />}
+        {param.indexId == 3 && <ContentSection3 index={param.indexId} />}
       </div>
       <div className="container5">
-        {param.indexId == 1 && (
-          <LinkSection LinkData={LinkData[param.indexId - 1]} />
-        )}
+        <LinkSection LinkData={LinkData[param.indexId - 1]} />
       </div>
     </>
   );
@@ -33,14 +33,35 @@ const LinkData = [
     {
       number: "01.",
       type: "LINK",
-      desc: "UI 디자인 작업물 (피그마)",
+      desc: ["UI 디자인 작업물"],
       link: "https://www.figma.com/file/wPyvTnfY3nNq3UDALngB8E/labrador-ui?type=design&node-id=0%3A1&mode=design&t=Wp7g5qND5cr0JOhn-1",
     },
     {
       number: "02.",
       type: "PDF",
-      desc: "인턴 최종 발표 'Wrap-Up' 자료",
+      desc: ["인턴 최종 발표", "'Wrap-Up' 자료"],
       link: "https://drive.google.com/file/d/16Ezy5y1aLDvElMocrznNWKuIKzVwwigD/view?usp=drive_link",
+    },
+  ],
+  [],
+  [
+    {
+      number: "01.",
+      type: "LINK",
+      desc: ["UI 디자인 작업물"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      number: "02.",
+      type: "PDF",
+      desc: ["파란학기제의 중간발표인", "'중간발표회' 자료"],
+      link: "https://drive.google.com/file/d/1St6e_hoHL8LMwJ4RhMiZ0ChGbvrxP-oj/view?usp=sharing",
+    },
+    {
+      number: "03.",
+      type: "PDF",
+      desc: ["파란학기제의 최종발표인", "'성과발표회' 자료"],
+      link: "https://drive.google.com/file/d/1ROT682ACHS08XL5N6hCrYxa7jzplxzyh/view?usp=sharing",
     },
   ],
 ];
