f2e467322ff4a0d1b164ca2246fba888a970bc14
hyeseung <wjoo3@naver.com>
2023-10-01 19:53:42 +0900

    feat : 페이지 이동시 스크롤 맨 위로 오게 하는 리액트 훅 생성

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import IndexPage from "./pages/IndexPage";
 import DetailPage from "./pages/DetailPage";
+import ScrollToTop from "./hooks/useScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/index" element={<IndexPage />} />
